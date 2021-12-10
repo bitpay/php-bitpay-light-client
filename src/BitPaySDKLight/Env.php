f10799ea9027000c45bedfb1986b618befc0f5c5
Antonio Buedo <agallardo@bitpay.com>
2021-12-10 10:33:28 +0100

    #v2.1.2112.0-rc - Api frame update (#16)

--- src/BitPaySDKLight/Env.php
+++ src/BitPaySDKLight/Env.php
@@ -11,7 +11,7 @@ interface Env
     const TestUrl               = "https://test.bitpay.com/";
     const ProdUrl               = "https://bitpay.com/";
     const BitpayApiVersion      = "2.0.0";
-    const BitpayPluginInfo      = "BitPay_PHP_Client_v2.0.2007";
-    const BitpayApiFrame        = "custom-light";
+    const BitpayPluginInfo      = "BitPay_PHP_Client_v2.1.2112";
+    const BitpayApiFrame        = "std";
     const BitpayApiFrameVersion = "1.0.0";
 }
